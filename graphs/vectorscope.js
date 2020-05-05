60d7ca944f368f8547f0d2fa556598cab8237214
dargonesti <migneault62@hotmail.com>
2020-05-05 09:42:41 -0400

    Updates

---
+++ graphs/vectorscope.js
@@ -0,0 +1,160 @@
+let scaleDraw = {
+    rgb: (histogramData, dest, destCtx, options, maxCount) => {
+        if (options.fill) {
+            destCtx.strokeStyle = "#000"
+        } else if (false) {
+            //...
+        }
+
+        for (var col = 0; col < 3; col++) {
+
+            if (col == 0)
+                destCtx.fillStyle = "#f00";
+            else if (col == 1)
+                destCtx.fillStyle = "#0f0";
+            else
+                destCtx.fillStyle = "#00f";
+
+            if (options.alias) {
+                destCtx.beginPath();
+                destCtx.moveTo(0, dest.height);
+            }
+
+            //histogramData [r|g|b|a] [0-255]
+            for (var x, y, i = 0; i <= 255; i++) {
+                var colData = histogramData[col];
+
+                y = Math.round((colData[i] / maxCount) * dest.height);
+                x = Math.round((i / 255) * dest.width);
+
+                if (options.alias) {
+                    destCtx.lineTo(x, dest.height - y);
+                } else if (plotStyle.value === 'discreet') {
+                    if (options.fill) {
+                        destCtx.fillRect(x, dest.height - y, discreetWidth, y);
+                    } else {
+                        destCtx.fillRect(x, dest.height - y, discreetWidth, 2);
+                    }
+                }
+            }
+
+            if (options.alias) {
+                destCtx.lineTo(x, dest.height);
+                if (options.fill) {
+                    destCtx.fill();
+                }
+                destCtx.stroke();
+                destCtx.closePath();
+            }
+        }
+    },
+    luma: () => {
+
+    },
+    HUE: () => {
+
+    },
+    HSL: () => {
+
+    }
+};
+
+/*
+  let histogramData = [];
+  histogramData.push(new Array(256).fill(0));
+  histogramData.push(new Array(256).fill(0));
+  histogramData.push(new Array(256).fill(0));
+  histogramData.push(new Array(256).fill(0));
+  imgData.forEach((v, i) => {
+    histogramData[i % 4][v]++;
+  });
+  //console.log(histogramData);
+*/
+
+var rgb2hsv = function (red, green, blue) {
+    red /= 255;
+    green /= 255;
+    blue /= 255;
+
+    var hue, sat, val,
+        min   = Math.min(red, green, blue),
+        max   = Math.max(red, green, blue),
+        delta = max - min,
+        val   = max;
+
+    // This is gray (red==green==blue)
+    if (delta === 0) {
+      hue = sat = 0;
+    } else {
+      sat = delta / max;
+
+      if (max === red) {
+        hue = (green -  blue) / delta;
+      } else if (max === green) {
+        hue = (blue  -   red) / delta + 2;
+      } else if (max ===  blue) {
+        hue = (red   - green) / delta + 4;
+      }
+
+      hue /= 6;
+      if (hue < 0) {
+        hue += 1;
+      }
+    }
+
+    return [Math.round(hue*360), Math.round(sat*100), Math.round(val*100)];
+  };
+
+
+function contextToVectorscope(sourceData, dest, destCtx, precision) {
+    let start = Date.now()
+    let options = {
+        alias: true,
+        fill: true,
+        scale: "rgb", // rgb, luma, HUE, Saturation
+        gradient: false
+    }; 
+    destCtx.clearRect(0, 0, dest.width, dest.height);
+
+    if (options.fill) {
+        destCtx.globalCompositeOperation = 'lighter';
+    }
+
+    // TODO : Draw circle : https://www.w3schools.com/tags/canvas_arc.asp
+
+    const startL = 25;
+    const incL = 10;
+    const centerX = dest.width / 2;
+    const centerY = dest.height / 2;
+    const minCenter = Math.min(centerX, centerY); // TODO : - margin
+    for(var i = 0; i< sourceData.length; i += 4 * Math.floor(100/precision)){
+      //  if(i > 32100) continue;
+
+        let hsl = rgb2hsv(sourceData[i], sourceData[i+1], sourceData[i+2]);
+
+        let dist = minCenter * (hsl[1] / 100) * ( hsl[2]/100);
+
+        let rotationGraph = 180 + 20; //20 pour vectorscopes
+        y = centerY + dist * Math.cos(Math.PI * (hsl[0]+rotationGraph)/180);
+        x = centerX + dist * Math.sin(Math.PI * (hsl[0]+rotationGraph)/180);
+
+        //destCtx.arc(centerX, centerY, minCenter * hsl[1] / 100 , hsl[0], hsl[0])
+
+        let curL = startL;
+        destCtx.fillStyle = `hsl(${hsl[0]}, 100%, ${curL}%)`;
+        //destCtx.stroke();
+        //histCtx.fillRect(x, histCanvas.height - y, discreetWidth, y );
+        destCtx.fillRect(x,y,1,1);
+    }
+
+    console.log("Done in : ", (Date.now()-start) / 1000 , "s on p:", precision)
+    /*
+        if (plotFill.checked && chans.length > 1) {
+          histCtx.globalCompositeOperation = 'source-over';
+        }*/
+
+}
+
+
+
+exports.contextToVectorscope = contextToVectorscope;
---
+++ render.js
@@ -0,0 +1,153 @@
+const { contextToHistogram } = require("./graphs/histogram.js");
+const { contextToVectorscope } = require("./graphs/vectorscope.js");
+const { desktopCapturer, remote } = require('electron');
+
+const { writeFile } = require('fs');
+
+
+const { dialog, Menu } = remote;
+
+// Global state
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Buttons
+const videoElement = document.querySelector('video');
+
+const canvasElement = document.querySelector('canvas#c');
+var histoElement = document.querySelector("#histo");
+var vectorElement = document.querySelector("#vectorscope");
+
+let videoCtx = canvasElement.getContext('2d');
+let histoCtx = histoElement.getContext('2d');
+let vectorCtx = vectorElement.getContext('2d');
+
+const startBtn = document.getElementById('startBtn');
+let animationId = -1;
+
+startBtn.onclick = e => {
+  if (animationId < 0)
+    animationId = setInterval(takeSnapshot, 1);
+};
+
+const precision = document.getElementById('precision');
+const stopBtn = document.getElementById('stopBtn');
+
+stopBtn.onclick = e => {
+  clearInterval(animationId);
+  animationId = -1;
+};
+
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const inputSources = desktopCapturer.getSources({
+  types: ['window', 'screen']
+})
+  .then(sources =>
+    selectSource(sources[0]));
+
+// Get the available video sources
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
+
+
+  videoOptionsMenu.popup();
+}
+
+// Change the videoSource window to record
+async function selectSource(source) {
+
+  videoSelectBtn.innerText = source.name;
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices
+    .getUserMedia(constraints);
+  //console.log(stream);
+  stream.onaddtrack = (track) => {
+    // console.log(track);
+  }
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
+
+  // Create the Media Recorder
+  const options = { mimeType: 'video/webm; codecs=vp9' };
+
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+
+  // Updates the UI
+}
+
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+  console.log('video data available');
+  recordedChunks.push(e.data);
+}
+
+// Saves the video file on stop
+async function handleStop(e) {
+  const blob = new Blob(recordedChunks, {
+    type: 'video/webm; codecs=vp9'
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: 'Save video',
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  if (filePath) {
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+  }
+
+}
+
+animationId = setInterval(takeSnapshot, 1);
+function takeSnapshot() {
+  var img = document.querySelector('img') || document.createElement('img');
+  var width = videoElement.offsetWidth
+    , height = videoElement.offsetHeight;
+
+  canvasElement.width = width;
+  canvasElement.height = height;
+
+  videoCtx.drawImage(videoElement, 0, 0, width, height);
+
+  let imgData = videoCtx.getImageData(0, 0, width, height).data; //[rgbargbargba...]
+
+  contextToHistogram(imgData, histoElement, histoCtx, parseInt(precision.value))
+  contextToVectorscope(imgData, vectorElement, vectorCtx, parseInt(precision.value))
+
+  delete imgData;
+
+  //img.src = canvasElement.toDataURL('image/png');
+  //document.body.appendChild(img);
+}
